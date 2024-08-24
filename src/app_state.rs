8f8c85331b6f14bed3fc3dd7bcee44637276b898
ozpv <39195175+ozpv@users.noreply.github.com>
2024-08-24 21:15:41 +0000

    Delete src/app_state.rs

--- src/app_state.rs
+++
@@ -1,9 +0,0 @@
-use axum::extract::FromRef;
-use leptos::LeptosOptions;
-use sqlx::{Pool, Postgres};
-
-#[derive(FromRef, Debug, Clone)]
-pub struct AppState {
-    pub db_pool: Pool<Postgres>,
-    pub leptos_options: LeptosOptions,
-}
